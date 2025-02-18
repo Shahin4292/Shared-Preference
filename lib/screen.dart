c77a60c2e2e47dcfe89ac9c8358a06465773c359
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-18 16:32:59 +0600

    add button

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Screen extends StatelessWidget {
-  const Screen({super.key});
+  Screen({super.key});
+
+  var nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [
-      TextField(),
-      ElevatedButton(onPressed: (){}, child: Text("Saved"))
-    ],),);
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+                labelText: "Name",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20))),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () async {
+                var name = nameController.text.toString();
+                var prefs = await SharedPreferences.getInstance();
+              },
+              child: Text("Saved")),
+          SizedBox(height: 10),
+          Text("Saved Value")
+        ],
+      ),
+    );
   }
 }
